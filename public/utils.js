e82e89711241ebf4897c39d34d0d1b3d0ef2cdc0
David Liu <shaowei.liu@gmail.com>
2013-12-29 11:33:50 +0800

    added static javascript utils

---
+++ public/utils.js
@@ -0,0 +1,47 @@
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+// Copied from underscore.js
+var debounce = function(func, wait) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      func.apply(context, args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+};
+
+uriSerialize = function(obj) {
+  var str = [];
+  for(var p in obj){
+    if (obj.hasOwnProperty(p)) {
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    }
+  }
+  return str.join("&");
+}
+
+postHTTP = function(path, params, onreadystatechange, async) {
+  var paramStr = uriSerialize(params);
+  var req = new XMLHttpRequest();
+  if (onreadystatechange != undefined) {
+    req.onreadystatechange = function(){onreadystatechange(req);};
+  }
+  req.open("POST", path, !!async);
+  req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  req.send(paramStr);
+}
+
+function randomString(length) {
+  var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var result = '';
+  for (var i = length; i > 0; --i) {
+    result += chars[Math.round(Math.random() * (chars.length - 1))];
+  }
+  return result;
+}
